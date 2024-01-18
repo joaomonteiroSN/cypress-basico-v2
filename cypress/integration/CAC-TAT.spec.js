0d139ee386d977043054c94cbab21a747b8bfeea
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2024-01-18 14:00:40 -0300

    exercicios cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,43 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function () {
+
+    beforeEach(function () {
+        cy.visit('./src/index.html')
+    });
+
+    it('verifica o título da aplicação', function () {
+
+        cy.title().should('equal', 'Central de Atendimento ao Cliente TAT');
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function () {
+
+        const longText = 'lorin loren loreanin loremaind lorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaindlorin loren loreanin loremaind'
+
+        cy.get('#firstName').type('João')
+        cy.get('#lastName').type('Monteiro')
+        cy.get('#email').type('joaomonteiroop@gmail.com')
+        cy.get('#open-text-area').type(longText, { delay: 0 })
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should("be.visible")
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+
+        cy.get('#firstName').type('João')
+        cy.get('#lastName').type('Monteiro')
+        cy.get('#email').type('joaomonteiroopgmail.com')
+        cy.get('#open-text-area').type('test')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it.only('campo telefone permanece vazio se for preenchido com valor nao-numerico', function () {
+        cy.get('#phone')
+            .type('abcdefghij')
+            .should('have.value', '')
+    })
+})
